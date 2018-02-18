875912270be375093d5fc3a1dc521daacf0efb75
Nelson Camilo <nelsonk355@gmail.com>
2018-02-17 20:16:08 -0600

    Using phpmailer

--- js/contact.js
+++ js/contact.js
@@ -2,6 +2,9 @@ $(function () {
 
     $("#contact-form").submit(function () {
 
+        var btn = $('#submit_message'), ctc_form = $('#contact-form');
+        btn.button('loading');
+
         var url = "mail.php"; 
 
         $.ajax({
@@ -10,21 +13,16 @@ $(function () {
             data: $(this).serialize(), 
             success: function (data)
             {
-                console.log(data);
-                var messageAlert = 'alert-' + data.type;
-                var messageText = data.message;
-
                 var alertBox = '<div class="alert alert-success alert-dismissable fade in success-message"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Your message has been successfully sent  </div>';
-                $('#contact-form').find('.messages').html(alertBox);
-                $('#contact-form')[0].reset();
+                ctc_form.find('.messages').html(alertBox);
+                ctc_form[0].reset();
+                btn.button('reset');
 
                 setTimeout(() => {
-                    // $('#contact-form').find('.messages').html('');
-                    $('#contact-form').find('.alert').fadeOut();
+                    ctc_form.find('.alert').fadeOut();
                 }, 3000);
             }
         });
         return false; 
     });
-
 });
--- mail.php
+++ mail.php
@@ -7,35 +7,46 @@ use PHPMailer\PHPMailer\Exception;
 //Load composer's autoloader
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+$mail = new PHPMailer(true);                                                    // Passing `true` enables exceptions
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');  // array variable name => Text to appear in email
+
+$emailText = "<h1>You have new message from contact form</h1><h1>=====================</h1>";
+
+foreach ($_POST as $key => $value) {
+
+    if (isset($fields[$key])) {
+        $emailText .= "<h3 style='text-transform: capitalize; margin-bottom: 0'>$fields[$key]:</h3><div>$value</div>";
+    }
+}
+
 try {
   //Server settings
   $mail->SMTPDebug = 2;                                 // Enable verbose debug output
   $mail->isSMTP();                                      // Set mailer to use SMTP
-  $mail->Host = 'mail.nelsonoi.com';  // Specify main and backup SMTP servers
+  $mail->Host = 'mail.nelsonoi.com';                    // Specify main and backup SMTP servers
   $mail->SMTPAuth = true;                               // Enable SMTP authentication
-  $mail->Username = 'nelson@nelsonoi.com';                 // SMTP username
-  $mail->Password = 'EMAILphp';                           // SMTP password
+  $mail->Username = 'nelson@nelsonoi.com';              // SMTP username
+  $mail->Password = 'EMAILphp';                         // SMTP password
   $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-  $mail->Port = 25;                                    // TCP port to connect to
+  $mail->Port = 25;                                     // TCP port to connect to
+
+  // https://stackoverflow.com/questions/3477766/phpmailer-smtp-error-could-not-connect-to-smtp-host
+  $mail->SMTPOptions = array(
+    'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+    )
+  );
 
   //Recipients
-  $mail->setFrom('nelson@nelsonoi.com', 'Nelson Orduz');
-  $mail->addAddress('ncoidev@gmail.com');     // Add a recipient
-  // $mail->addAddress('ellen@example.com');               // Name is optional
-  // $mail->addReplyTo('info@example.com', 'Information');
-  // $mail->addCC('cc@example.com');
-  // $mail->addBCC('bcc@example.com');
-
-  //Attachments
-  // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-  // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+  $mail->setFrom('nelson@nelsonoi.com', $_POST['name']);
+  $mail->addAddress('ncoidev@gmail.com');                // Add a recipient
 
   //Content
-  $mail->isHTML(true);                                  // Set email format to HTML
-  $mail->Subject = 'Here is the subject';
-  $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+  $mail->Subject = 'New email from nelsonoi.com';
+  $mail->isHTML(true);                                   // Set email format to HTML    
+  $mail->Body = $emailText;
 
   $mail->send();
   echo 'Message has been sent';
